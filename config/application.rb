2197e13792ec3195ce933c7f16653858fbe68a46
BachShinichiroe <kitchen.number@gmail.com>
2020-09-23 15:19:44 +0900

    タイムゾーンを日本に設定

--- config/application.rb
+++ config/application.rb
@@ -4,8 +4,13 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
+config.time_zone = 'Tokyo'
+config.active_record.default_timezone = :local
+
 module ManyoTask
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.load_defaults 5.2
     config.generators do |g|
       g.test_framework :rspec,
