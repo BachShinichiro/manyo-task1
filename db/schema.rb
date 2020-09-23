c51d4fc8b50af2cc377820289bb12bb7e2d768fe
BachShinichiroe <kitchen.number@gmail.com>
2020-09-23 13:34:35 +0900

    Rspecテスト実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_225351) do
+ActiveRecord::Schema.define(version: 2020_09_18_054418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.string "content"
+    t.string "name", null: false
+    t.string "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
