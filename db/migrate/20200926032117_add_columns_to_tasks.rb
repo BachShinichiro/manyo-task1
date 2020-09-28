f7fa047ba66d49cde82979985cff7ccf418173b7
BachShinichiroe <kitchen.number@gmail.com>
2020-09-28 21:33:58 +0900

    ステータスのデータ型をintegerに変更

--- db/migrate/20200926032117_add_columns_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnsToTasks < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tasks, :status, :string, default: '未着手', null: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_034115) do
+ActiveRecord::Schema.define(version: 2020_09_28_103855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 2020_09_28_034115) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.date "limit", default: -> { "now()" }, null: false
-    t.string "status", default: "未着手", null: false
     t.integer "priority", default: 0, null: false
+    t.integer "status", default: 0, null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
