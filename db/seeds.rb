fa645f6abf26a57b9802f61cd41c59fc2ae2e367
BachShinichiroe <kitchen.number@gmail.com>
2020-10-07 20:16:15 +0900

    シードデータ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# User.create!(name: 'バッハ',
-#   email: 'kitchen.number@gmail.com',
-#   password: 'password',
-#   password_confirmation: 'password',
-#   admin: true
-#   )
+User.create!(name: 'バッハ',
+  email: 'kitchen.number@gmail.com',
+  password: 'password',
+  password_confirmation: 'password',
+  admin: true
+  )
 
 10.times do |n|
   name = Faker::Games::Pokemon.name
@@ -13,7 +13,7 @@
                 email: email,
                 password: password,
                 password_confirmation: password,
-                admin: false,
+                admin: false
                 )
 end
 
@@ -23,13 +23,13 @@ end
   status = rand(0..2)
   priority = rand(0..2)
   limit = Faker::Date.between(from: '2020-09-28', to: '2020-12-31')
-  user_id = 1
+  user_id = User.last.id
   Task.create!(name: name,
                content: content,
                status: status,
                limit: limit,
                priority: priority,
-               user_id:  1,
+               user_id:  user_id
                )
 end
 
